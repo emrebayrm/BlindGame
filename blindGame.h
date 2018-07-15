e37dd79292233f5dadfd297f7b795e0cfc52e02f
Sincap <Sincap@Sincap-MacBook-Pro.local>
2018-07-15 14:20:52 +0300

    interface update

--- blindGame.h
+++ blindGame.h
@@ -12,19 +12,40 @@
 #include <stdio.h>
 #include "point.h"
 #include "player.h"
+#include "goal.h"
 
 typedef struct {
     char **map;
+    int mapRow;
+    int mapCol;
     int maxPlayer;
     int currPlayers;
-    Point goalPoint;
+    char blocks[5];
+    Point *goalPoint;
     Player *players;
     int id;
 } BlindGame;
 
-int isValidDir(Point *p, int dir, char **map, int mapRow, int mapCol);
+void moveObject(BlindGame *game, int dir, void *movable, void (*moveOp) (void*));
 
-void movePlayer(Player *player, int dir, char **map);
+void movePlayer(BlindGame *game, int dir, Player *player);
+void moveGoal(BlindGame *game, int dir, Goal *goal);
+
+//returns -1 if the point is out of bounds, 0 otherwise
+int checkBounds(BlindGame *game, Point p);
+
+//returns -1 if the point is valid, 0 otherwise
+int isValidPoint(BlindGame *game, Point p);
+
+//generates the map
+void generateMap(BlindGame *game);
+
+//finds a random valid location in the map
+Point* getRandomPoint(BlindGame *game);
+
+//places the goal
+void placeGoal(BlindGame *game);
+
 
 
 
