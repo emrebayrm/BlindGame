7d799377f8718c4940a573d4eca86b18aa21d06e
Sincap <Sincap@Sincap-MacBook-Pro.local>
2018-07-15 14:19:52 +0300

    interface update

---
+++ goal.c
@@ -0,0 +1,9 @@
+//
+//  goal.c
+//  BlindGame
+//
+//  Created by Sincap on 15.07.2018.
+//  Copyright © 2018 Sincap. All rights reserved.
+//
+
+#include "goal.h"
---
+++ goal.h
@@ -0,0 +1,14 @@
+//
+//  goal.h
+//  BlindGame
+//
+//  Created by Sincap on 15.07.2018.
+//  Copyright © 2018 Sincap. All rights reserved.
+//
+
+#ifndef goal_h
+#define goal_h
+
+#include <stdio.h>
+
+#endif /* goal_h */
