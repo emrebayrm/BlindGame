8409977799139dc1705b3ac20888097f64c1f88f
Emre Bayram <e.bayram@gtu.edu.tr>
2018-07-17 10:56:20 +0300

    Network Module update

---
+++ NetworkModule.cpp
@@ -0,0 +1,32 @@
+//
+// Created by patates on 7/17/18.
+//
+
+#include "NetworkModule.h"
+
+void ServerNetworkModule::init(int port) {
+
+}
+
+int ServerNetworkModule::send(void *buf, int size) {
+    return 0;
+}
+
+int ServerNetworkModule::recv(void *buf, int size) {
+    return 0;
+}
+
+
+
+
+int ClientNetworkModule::send(void *buf, int size) {
+    return 0;
+}
+
+void ClientNetworkModule::init(int port) {
+
+}
+
+int ClientNetworkModule::recv(void *buf, int size) {
+    return 0;
+}
