a149ae2cb744dfac3ae391594da6b938059aee93
kowkon <sevindikberat@gmail.com>
2018-07-17 21:06:24 +0300

    sincap

---
+++ coin.cpp
@@ -0,0 +1,9 @@
+//
+//  coin.cpp
+//  BlindGame
+//
+//  Created by Sincap on 17.07.2018.
+//  Copyright © 2018 Sincap. All rights reserved.
+//
+
+#include "coin.hpp"
---
+++ coin.hpp
@@ -0,0 +1,14 @@
+//
+//  coin.hpp
+//  BlindGame
+//
+//  Created by Sincap on 17.07.2018.
+//  Copyright © 2018 Sincap. All rights reserved.
+//
+
+#ifndef coin_hpp
+#define coin_hpp
+
+#include <stdio.h>
+
+#endif /* coin_hpp */
