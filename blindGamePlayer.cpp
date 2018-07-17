9a85ca5c62edbe9069d86a357f657f4ecb25d2f5
Sincap <Sincap@Sincap-MacBook-Pro.local>
2018-07-17 15:22:02 +0300

    hi

---
+++ blindGamePlayer.cpp
@@ -0,0 +1,9 @@
+//
+//  blindGamePlayer.cpp
+//  BlindGame
+//
+//  Created by Sincap on 17.07.2018.
+//  Copyright © 2018 Sincap. All rights reserved.
+//
+
+#include "blindGamePlayer.hpp"
---
+++ blindGamePlayer.hpp
@@ -0,0 +1,14 @@
+//
+//  blindGamePlayer.hpp
+//  BlindGame
+//
+//  Created by Sincap on 17.07.2018.
+//  Copyright © 2018 Sincap. All rights reserved.
+//
+
+#ifndef blindGamePlayer_hpp
+#define blindGamePlayer_hpp
+
+#include <stdio.h>
+
+#endif /* blindGamePlayer_hpp */
