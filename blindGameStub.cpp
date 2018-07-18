6c8c681107002255805bf6a676ce017e1247ca95
kowkon <sevindikberat@gmail.com>
2018-07-18 16:17:58 +0300

    mqtt

--- blindGameStub.cpp
+++ blindGameStub.cpp
@@ -12,11 +12,14 @@
 
 using namespace std;
 
+/*
+
 int main(void) {
     BlindGame *game = new BlindGame(0, 3, "myGame");
     
     game->join("emre");
     game->join("oguz");
+    game->join("sincap");
     game->join("sincap");
     
     game->startGame();
@@ -27,7 +30,7 @@ int main(void) {
     while((win = game->isFinished()) == -1) {
         cout << "coinLocation: x: " << game->getCoinLocation()->getX() << " y: " << game->getCoinLocation()->getY() << endl;
         vector<pair<int,int>> dists = game->getCoinDistances();
-        cout << endl << "***distances***" << endl;
+        cout << "***distances***" << endl;
         for(int i = 0; i < dists.size(); ++i) {
             cout << "playerId: " << dists[i].first << " dist: " << dists[i].second << endl;
         }
@@ -36,14 +39,18 @@ int main(void) {
             BlindGamePlayer *player = ((BlindGamePlayer*) game->getPlayers()[i]);
             cout << player->getId() << "- " << player->getName() << " " << player->getLocation()->getX() << " " << player->getLocation()->getY() << "  " << player->getCurrMove() << "/"  << player->maxMove << endl;
         }
+        cout << "**********" << endl;
         cout << "enter playerId to play" << endl;
         cin >> playerId;
         cout << "enter direction (0-UP , 1-Rigth, 2-down, 3-left)" << endl;
         cin >> dir;
         
+        
         game->movePlayer(dir, playerId);
         
     }
-    cout << win << " playerId Wins!!!!!!!" << endl;
+    cout << win << " playerId Wins!!!!!!!" << endl << endl;
     return 0;
 }
+ 
+ */
--- dummies/DummyMqttTest.cpp
+++ dummies/DummyMqttTest.cpp
@@ -2,6 +2,24 @@
 // Created by patates on 7/18/18.
 //
 
+#include "mqttHandler.h"
+#include <string>
+
+#include <iostream>
+
+using namespace std;
+
 int main(){
+    
+    mqttPublisher *pub = new mqttPublisher("topic", "192.168.2.236", "124123");
+    pub->init();
+    pub->publish("asdkasdoa");
+    
+    mqttSubscriber *sub = new mqttSubscriber("topic", "192.168.2.236", "1324312");
+    sub->init();
+    char buf[10];
+    sub->receive(buf);
+    
+    cout << buf << endl;
 
 }
