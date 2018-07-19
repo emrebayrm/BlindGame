efa6b3bd66ee1e4a1bde754c15f794bdd341bf0c
Emre Bayram <e.bayram@gtu.edu.tr>
2018-07-19 16:12:49 +0300

    Auto stash before merge of "blindcpp" and "origin/blindcpp"

--- BlindInterface/mainwindow.cpp
+++ BlindInterface/mainwindow.cpp
@@ -9,11 +9,33 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    printf("asd");
+    thread = new communucationThread(this);
+    printf("asd");
+
+    connect(thread,SIGNAL(updateUi(int,int)),this,SLOT(updatethisUi(int,int)));
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+void MainWindow::init(string topic,string address,string clientid){
+    sender = new mqttPublisher(topic,address,clientid);
+
+    thread->setMqttOptions(topic,address,clientid);
+    thread->init();
+
+    thread->run();
+    sender->init();
+
+}
+
+void MainWindow::updatethisUi(int x,int y){
+    location.setX(x);
+    location.setY(y);
+    repaint();
 }
 
 void MainWindow::paintEvent(QPaintEvent *e) {
@@ -33,4 +55,40 @@ void MainWindow::paintEvent(QPaintEvent *e) {
 
 
 
+}
+
+void MainWindow::on_UpBut_clicked()
+{
+    string message;
+    message.append(to_string(id));
+    message.append(",");
+    message.append("0");
+    sender->publish(message);
+}
+
+void MainWindow::on_LeftBut_clicked()
+{
+    string message;
+    message.append(to_string(id));
+    message.append(",");
+    message.append("3");
+    sender->publish(message);
+}
+
+void MainWindow::on_RightBut_clicked()
+{
+    string message;
+    message.append(to_string(id));
+    message.append(",");
+    message.append("1");
+    sender->publish(message);
+}
+
+void MainWindow::on_DownBut_clicked()
+{
+    string message;
+    message.append(to_string(id));
+    message.append(",");
+    message.append("2");
+    sender->publish(message);
 }
